9b227c3d3d9c084e2d3af4d5808c46e2c493faa1
mlkone <martin.kuenzel@hs-bochum.de>
2019-06-28 14:21:15 +0200

    Ausbau der blogseite um details für posts, generic view classes

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name = 'blog-index')
+    path('',views.PostListView.as_view(), name = 'blog-index'),
+    path('<int:pk>/details',views.DetailView.as_view(), name = 'blog-post-details')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Post
 
 # Create your views here.
-def index(req):
-    context = {
-        'title': 'Blog`s',
-        'posts': Post.objects.all()
-    }
-    return render(req,'blog/index.html',context)
+class PostListView(generic.ListView):
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Post
+
