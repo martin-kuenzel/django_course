d954075b7afb173c2db1f1f3b05c911683a6e456
mlkone <martin.kuenzel@hs-bochum.de>
2019-06-28 14:32:18 +0200

    styles auskommentiert, adminpages für blog ausgebaut und optimiert

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','date_created','date_updated','was_created_recently','was_updated_recently')
+    list_filter = ['date_created']
+    search_fields = ['title']
+
+    fieldsets = [
+        ( None, { 'fields': ['title'] } ),
+        ( 'Erstellungsdatum', { 'fields': ['date_created'], 'classes': ['collapse'] } ),
+        ( 'Aktualisierungsdatum', { 'fields': ['date_updated'], 'classes': ['collapse'] } ),
+        ( 'Inhalt', { 'fields': ['content'] } )
+    ]
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,27 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_created = models.DateTimeField('date_published')
-    date_updated = models.DateTimeField(default=datetime.now)
+    date_updated = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'{self.title} {self.date_created}'
+
+    def was_created_recently(self):
+        return timezone.now() >= self.date_created >= timezone.now() - datetime.timedelta(days=1)
+
+    was_created_recently.admin_order_field = 'date_created'
+    was_created_recently.boolean = True
+    was_created_recently.short_description = 'Created recently'
+    
+    def was_updated_recently(self):
+        return self.date_updated >= timezone.now() - datetime.timedelta(days=1)
+
+    was_updated_recently.admin_order_field = 'date_updated'
+    was_updated_recently.boolean = True
+    was_updated_recently.short_description = 'Updated recently'
