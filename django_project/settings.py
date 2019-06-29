f8f0f61145af0e2df049931742597a2b6a0f450d
mlkone <martin.kuenzel@hs-bochum.de>
2019-06-29 14:35:03 +0200

    password_reset eingebaut via gmail.com (EMAIL_PW, EMAIL_USR für Versand in activate als ENVIRONMENT_VARIABLES)

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,3 +122,13 @@ LOGIN_REDIRECT_URL = '/'
 # THE LOGIN FORM 
 LOGIN_URL = 'users/login'
 #LOGOUT_REDIRECT_URL = '/'
+
+#PASSWORD RESET STUFF
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_USR') #os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PW') #os.environ.get('EMAIL_PASS')
